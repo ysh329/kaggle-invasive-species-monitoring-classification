d2d0c6b9e05400bf2e8af9143e1ae91a803780f6
Shuai Yuan <ysh329@sina.com>
2017-07-21 11:46:50 +0000

    update

--- train-or-finetune-model/finetune-models/check_layer.py
+++ train-or-finetune-model/finetune-models/check_layer.py
@@ -6,4 +6,7 @@ import mxnet as mx
 load_model_prefix = sys.argv[1]
 load_model_epoch = int(sys.argv[2])
 sym, arg_params, aux_params = mx.model.load_checkpoint(load_model_prefix, load_model_epoch)
-print sym.get_internals().list_outputs()
+outputs_layers = sym.get_internals().list_outputs()
+for idx in xrange(len(outputs_layers)):
+	print idx+1, outputs_layers[idx]
+    
