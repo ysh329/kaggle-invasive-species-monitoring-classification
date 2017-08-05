7ec59f42023f922dc08a8b8bb600a6fd41d84dbe
Shuai Yuan <ysh329@sina.com>
2017-08-05 06:18:12 +0000

    update get_result_for_one_network.sh

--- train-or-finetune-model/get_result_for_one_network.sh
+++ train-or-finetune-model/get_result_for_one_network.sh
@@ -6,10 +6,17 @@
 #model_prefix_arr=("../models/pretrained-models/vgg-19/vgg19-train-224-lr-0.001")
 #model_prefix_arr=("../models/pretrained-models/nin/nin-train-224-lr-0.001-momentum-0.9")
 #model_prefix_arr=("../models/pretrained-models/resnext-101/resnext-101-train-224-lr-0.01")
-
-model_prefix_arr=("../models/pretrained-models/squeezenet-v1.0/squeezenet_v1.0-train-224-lr-0.001-momentum-0.9")
-
+#model_prefix_arr=("../models/pretrained-models/squeezenet-v1.0/squeezenet_v1.0-train-224-lr-0.001-momentum-0.9")
 #model_prefix_arr=("../models/pretrained-models/squeezenet-v1.1/squeezenet_v1.1-train-224-lr-0.001-momentum-0.9")
+
+
+model_prefix_arr=("../models/pretrained-models/inception-21k/inception-21k-train-224-lr-0.01-momentum-0.9")
+
+#model_prefix_arr=("../models/pretrained-models/inception-bn/Inception-BN-train-224-lr-0.01-momentum-0.9")
+
+## un fintune
+# ../models/pretrained-models/inception-v3
+# ../models/pretrained-models/inception-bn-126
 
 model_max_epoch_num_arr=(100)
 #model_num=${#model_prefix_arr[@]}-1
