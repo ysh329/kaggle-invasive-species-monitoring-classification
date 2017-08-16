b76b6b2891998d1c2d7d575810943e3314afd188
Shuai Yuan <ysh329@sina.com>
2017-08-16 03:20:30 +0000

    add download mxnet format data

---
+++ data/download-mxnet-format-data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -c https://doc-0s-0s-docs.googleusercontent.com/docs/securesc/f8p0noiuins2en6qpsiogp38ldlgou7n/1jgoo0a085ukhlm59cp4ftmjeam6ib69/1502848800000/06880897897823082226/06880897897823082226/0B22osw8F0DNyai1CZGpkNmg5SnM?e=download&nonce=6ak4f3f7i9uea&user=06880897897823082226&hash=k96tl1jbuta8ue2rjcj443sontlnl1ti
