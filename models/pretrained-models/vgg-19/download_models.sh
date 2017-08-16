90cd033d37d08c7b16fb811bf22b102df8c044c7
root <ysh329@sina.com>
2017-08-16 13:13:10 +0000

    update

--- models/pretrained-models/vgg-19/download_models.sh
+++ models/pretrained-models/vgg-19/download_models.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-wget -c http://data.dmlc.ml/models/imagenet/caffenet/caffenet-0000.params
-wget -c http://data.dmlc.ml/models/imagenet/caffenet/caffenet-symbol.json
-
-wget -c http://data.dmlc.ml/models/imagenet/resnet/152-layers/resnet-152-0000.params
-wget -c http://data.dmlc.ml/models/imagenet/resnet/152-layers/resnet-152-symbol.json
-
-wget -c http://data.dmlc.ml/models/imagenet/vgg/vgg16-0000.params
-wget -c http://data.dmlc.ml/models/imagenet/vgg/vgg16-symbol.json
-
 wget -c http://data.dmlc.ml/models/imagenet/vgg/vgg19-0000.params
 wget -c http://data.dmlc.ml/models/imagenet/vgg/vgg19-symbol.json
